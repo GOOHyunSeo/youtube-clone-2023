ccf97ce73a4577429f0c0ab264bf125a78cb6b9f
GOOHyunSeo <ghs3956@naver.com>
2023-09-20 18:45:25 +0900

    Install morgan

--- src/server.js
+++ src/server.js
@@ -1,26 +1,14 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 const port = 4000;
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-const protectormiddleware = (req, res, next) => {
-  if (req.url === "/protect") {
-    res.send("<h1>not allowed</h1>");
-  }
-  next();
-};
 const home = (req, res) => res.send("Hello World!");
 
 app.use(logger);
-app.use(protectormiddleware);
 app.get("/", home);
-app.get("/protect", (req, res) => {
-  res.send("this is protect");
-});
 
 const handleListening = () => {
   console.log(`✅Server listening on port http://localhost:${port}`);
